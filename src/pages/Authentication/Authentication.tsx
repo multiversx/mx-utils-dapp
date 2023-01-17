58e8048944f1c673fbb165ed755530995e5ab9f2
Miro Mărgineanu <miro.margineanu@elrond.com>
2023-01-17 09:50:23 +0200

    Add network switch functionality

--- src/pages/Authentication/Authentication.tsx
+++ src/pages/Authentication/Authentication.tsx
@@ -1,16 +1,21 @@
 import { useState } from 'react';
+import { getChainID } from '@multiversx/sdk-dapp/utils/network';
+import { getEnvironmentForChainId } from '@multiversx/sdk-dapp/apiCalls/configuration/getEnvironmentForChainId';
 
 import { Template } from 'components/Template';
 
 import { Input } from './components/Input';
-import { Generate } from './components/Generate';
 import { Metric } from './components/Metric';
+import { Generate } from './components/Generate';
+import { Environment } from './components/Environment';
 
 import { TokenColorsEnum } from './enum';
 
-import type { MetricItemType, MetricType } from './types';
+import type { DefaultMetricType, MetricItemType, MetricType } from './types';
 
 import styles from './styles.module.scss';
+import { EnvironmentsEnum } from '@multiversx/sdk-dapp/types';
+import { useLocation } from 'react-router-dom';
 
 export const emptyMetrics: MetricType = {
   address: '',
@@ -22,15 +27,40 @@ export const emptyMetrics: MetricType = {
   extraInfo: {}
 };
 
-export const defaultMetrics: MetricType = {
-  address: 'erd1wjytfn6zhqfcsejvhwv7q4usazs5ryc3j8hc78fldgjnyct8wejqkasunc',
-  blockHash: 'a71697208c89c1e020697574a48ec1666022872d70273e09c67a895f12ba407b',
-  signature:
-    '53121b33a2b1d95fe5a016588b8cdb3a17bfec55f2b39e977ff0c468dc0ceed53375344abb13c1f2ee819eef425c0a3f0202e9ced01bf195a74e9066e956da00',
-  body: 'bG9jYWxob3N0.a71697208c89c1e020697574a48ec1666022872d70273e09c67a895f12ba407b.86400.eyJ0aW1lc3RhbXAiOjE2NzIyMzA1Mjh9',
-  host: 'localhost',
-  ttl: 86400,
-  extraInfo: { timestamp: 1672230528 }
+export const defaultMetrics: DefaultMetricType = {
+  [EnvironmentsEnum.mainnet]: {
+    address: 'erd1wjytfn6zhqfcsejvhwv7q4usazs5ryc3j8hc78fldgjnyct8wejqkasunc',
+    blockHash:
+      'a71697208c89c1e020697574a48ec1666022872d70273e09c67a895f12ba407b',
+    signature:
+      '53121b33a2b1d95fe5a016588b8cdb3a17bfec55f2b39e977ff0c468dc0ceed53375344abb13c1f2ee819eef425c0a3f0202e9ced01bf195a74e9066e956da00',
+    body: 'bG9jYWxob3N0.a71697208c89c1e020697574a48ec1666022872d70273e09c67a895f12ba407b.86400.eyJ0aW1lc3RhbXAiOjE2NzIyMzA1Mjh9',
+    host: 'localhost',
+    ttl: 86400,
+    extraInfo: { timestamp: 1672230528 }
+  },
+  [EnvironmentsEnum.devnet]: {
+    address: 'erd1wjytfn6zhqfcsejvhwv7q4usazs5ryc3j8hc78fldgjnyct8wejqkasunc',
+    blockHash:
+      'a71697208c89c1e020697574a48ec1666022872d70273e09c67a895f12ba407b',
+    signature:
+      '53121b33a2b1d95fe5a016588b8cdb3a17bfec55f2b39e977ff0c468dc0ceed53375344abb13c1f2ee819eef425c0a3f0202e9ced01bf195a74e9066e956da00',
+    body: 'bG9jYWxob3N0.a71697208c89c1e020697574a48ec1666022872d70273e09c67a895f12ba407b.86400.eyJ0aW1lc3RhbXAiOjE2NzIyMzA1Mjh9',
+    host: 'localhost',
+    ttl: 86400,
+    extraInfo: { timestamp: 1672230528 }
+  },
+  [EnvironmentsEnum.testnet]: {
+    address: 'erd1wjytfn6zhqfcsejvhwv7q4usazs5ryc3j8hc78fldgjnyct8wejqkasunc',
+    blockHash:
+      'a71697208c89c1e020697574a48ec1666022872d70273e09c67a895f12ba407b',
+    signature:
+      '53121b33a2b1d95fe5a016588b8cdb3a17bfec55f2b39e977ff0c468dc0ceed53375344abb13c1f2ee819eef425c0a3f0202e9ced01bf195a74e9066e956da00',
+    body: 'bG9jYWxob3N0.a71697208c89c1e020697574a48ec1666022872d70273e09c67a895f12ba407b.86400.eyJ0aW1lc3RhbXAiOjE2NzIyMzA1Mjh9',
+    host: 'localhost',
+    ttl: 86400,
+    extraInfo: { timestamp: 1672230528 }
+  }
 };
 
 /*
@@ -38,8 +68,16 @@ export const defaultMetrics: MetricType = {
  */
 
 export const Authentication = () => {
+  const location = useLocation();
+  const entries = Object.fromEntries(new URLSearchParams(location.search));
+  const network = entries.network as EnvironmentsEnum;
+
+  const [chain, setChain] = useState<EnvironmentsEnum>(
+    network || getEnvironmentForChainId(getChainID())
+  );
+
   const [show, setShow] = useState(false);
-  const [metrics, setMetrics] = useState<MetricType>(defaultMetrics);
+  const [metrics, setMetrics] = useState<MetricType>(defaultMetrics[chain]);
 
   const metricItems: MetricItemType[] = [
     {
@@ -106,17 +144,21 @@ export const Authentication = () => {
 
   return (
     <Template>
-      <Generate show={show} setShow={setShow} />
+      <Generate show={show} setShow={setShow} chain={chain} />
 
       <div className={styles.authentication}>
         <div className={styles.left}>
           <h2 className={styles.subtitle}>Encoded</h2>
 
-          <Input setMetrics={setMetrics} setShow={setShow} />
+          <Input setMetrics={setMetrics} setShow={setShow} chain={chain} />
         </div>
 
         <div className={styles.right}>
-          <h2 className={styles.subtitle}>Decoded</h2>
+          <h2 className={styles.subtitle}>
+            <span>Decoded</span>
+
+            <Environment chain={chain} setChain={setChain} />
+          </h2>
 
           {metrics && (
             <div className={styles.metrics}>
--- src/pages/Authentication/types.ts
+++ src/pages/Authentication/types.ts
@@ -1,8 +1,16 @@
 import type { ReactNode } from 'react';
+import type { EnvironmentsEnum } from '@multiversx/sdk-dapp/types';
 import type { NativeAuthDecoded } from '@multiversx/sdk-native-auth-server/lib/src/entities/native.auth.decoded';
 import type { TokenColorsEnum } from './enum';
 
 export type MetricType = NativeAuthDecoded | undefined;
+
+export interface DefaultMetricType {
+  [key: string]: MetricType;
+  [EnvironmentsEnum.mainnet]: MetricType;
+  [EnvironmentsEnum.devnet]: MetricType;
+  [EnvironmentsEnum.testnet]: MetricType;
+}
 
 export interface MetricItemType {
   name: string;
