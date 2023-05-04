f37543579ca012b00bd04f67264a7524b450440c
Theodor Dumitrache <theodor.dumitrache@yahoo.com>
2023-05-04 12:20:22 +0300

    added an additional error message for displaying token status for nativeAuth token

--- src/pages/Authentication/components/Input/components/Status/Status.tsx
+++ src/pages/Authentication/components/Input/components/Status/Status.tsx
@@ -12,25 +12,37 @@ import styles from './styles.module.scss';
 export const Status = (props: FormikProps<FormValuesType>) => {
   const { errors, values } = props;
 
-  const isError = useMemo(
+  const isTokenError = useMemo(
     () => Boolean(errors.token) || !Boolean(values.token),
     [values.token, errors.token]
   );
 
-  return (
-    <div
-      className={styles.status}
-      style={{
-        color: isError ? TokenColorsEnum.body : TokenColorsEnum.signature
-      }}
-    >
-      <span className={styles.icon}>
-        <FontAwesomeIcon icon={isError ? faTimes : faCheck} size='xs' />
-      </span>
+  const isMessageError = errors.message;
 
-      <span className={styles.text}>
-        {isError ? errors.token : 'Token Valid'}
-      </span>
+  return (
+    <div className={styles.statusContainer}>
+      <div
+        className={styles.status}
+        style={{
+          color: isTokenError ? TokenColorsEnum.body : TokenColorsEnum.signature
+        }}
+      >
+        <span className={styles.icon}>
+          <FontAwesomeIcon icon={isTokenError ? faTimes : faCheck} size='xs' />
+        </span>
+
+        <span className={styles.text}>
+          {isTokenError ? errors.token : 'Token Valid'}
+        </span>
+      </div>
+      {isMessageError && (
+        <span
+          className={styles.message}
+          style={{ color: TokenColorsEnum.body }}
+        >
+          {errors.message}
+        </span>
+      )}
     </div>
   );
 };
