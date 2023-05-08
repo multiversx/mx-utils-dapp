d435f30d86a2b1a4abf7259729af9081f604e882
Theodor Dumitrache <theodor.dumitrache@yahoo.com>
2023-05-08 23:19:06 +0300

    Sign Message tab -> sign-verify flows

---
+++ src/pages/SignMessage/SignMessage.tsx
@@ -0,0 +1,110 @@
+import React, { useEffect, useState } from 'react';
+import { Template } from 'components/Template';
+import { useSearchParams } from 'react-router-dom';
+
+import { useSignMessage } from '@multiversx/sdk-dapp/hooks/signMessage/useSignMessage';
+import { useGetSignMessageInfoStatus } from '@multiversx/sdk-dapp/hooks/signMessage/useGetSignedMessageStatus';
+
+import { logout } from '@multiversx/sdk-dapp/utils/logout';
+
+import styles from './styles.module.scss';
+import { Generate } from 'pages/Authentication/components/Generate';
+import {
+  SignMessageForm,
+  VerifySignatureForm,
+  VerifySignatureFormFields
+} from './components';
+import {
+  InitialVerifyFormValuesEnum,
+  InitialVerifyFormValuesType,
+  SignedMessageStatusesEnum
+} from './types';
+
+export const SignMessage = () => {
+  const { signMessage } = useSignMessage();
+  const { isPending } = useGetSignMessageInfoStatus();
+
+  const [messageToSign, setMessageToSign] = useState('');
+  const [signatureFromSignedMessage, setSignatureFromSignedMessage] =
+    useState('');
+  const [showProvidersModal, setShowProvidersModal] = useState(false);
+  const [initialVerifyFormValues, setInitialVerifyFormValues] =
+    useState<InitialVerifyFormValuesType>({
+      message: '',
+      signature: '',
+      address: ''
+    });
+
+  const [searchParams] = useSearchParams();
+
+  const handleSignMessage = async () => {
+    const signableMessage = await signMessage({
+      message: messageToSign
+    });
+
+    if (!signableMessage) {
+      return;
+    }
+
+    const hexSignature = signableMessage.getSignature().toString('hex');
+
+    setSignatureFromSignedMessage(hexSignature);
+
+    if (!isPending) {
+      logout();
+    }
+  };
+
+  const populateInitialVerifyFormFields = () => {
+    const addressParam =
+      searchParams.get(InitialVerifyFormValuesEnum.address) ?? '';
+    const messageParam =
+      searchParams.get(InitialVerifyFormValuesEnum.message) ?? '';
+    const signatureParam =
+      searchParams.get(InitialVerifyFormValuesEnum.signature) ?? '';
+    const signStatusParam = searchParams.get('status');
+
+    const isMessageSigned =
+      signStatusParam === SignedMessageStatusesEnum.signed;
+
+    setInitialVerifyFormValues({
+      address: addressParam,
+      message: messageParam,
+      signature: signatureParam
+    });
+
+    // This will set the signature in SignMessageForm after signing the message with web wallet
+    if (signatureParam && isMessageSigned) {
+      setSignatureFromSignedMessage(signatureParam);
+    }
+  };
+
+  useEffect(populateInitialVerifyFormFields, [searchParams]);
+
+  return (
+    <>
+      <Template>
+        <Generate
+          show={showProvidersModal}
+          setShow={setShowProvidersModal}
+          callbackAfterLogin={handleSignMessage}
+        />
+        <div className={styles.container}>
+          <SignMessageForm
+            setShow={setShowProvidersModal}
+            signature={signatureFromSignedMessage}
+            setSignature={setSignatureFromSignedMessage}
+            setMessage={setMessageToSign}
+          />
+
+          <VerifySignatureForm />
+
+          <VerifySignatureFormFields
+            initialVerifyFormValues={initialVerifyFormValues}
+          />
+        </div>
+      </Template>
+      ;
+    </>
+  );
+};
