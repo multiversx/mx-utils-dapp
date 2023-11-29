3d17ca749e79b38624dc1c257c233af71e9fb13c
cipriandraghici <cipriandraghici93@gmail.com>
2023-11-29 12:18:29 +0200

    remove log

--- src/hooks/useChain.ts
+++ src/hooks/useChain.ts
@@ -2,8 +2,5 @@ import { useGlobalContext } from 'context';
 
 export const useChain = () => {
   const { dappEnvironment: chain } = useGlobalContext();
-
-  console.log('chain', chain);
-
   return { chain };
 };
