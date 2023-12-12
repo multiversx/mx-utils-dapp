5ed9ff9b497254bc7ac20d8cbe2a18f08234c4d0
tudorgabriel <homoranu.tudor@gmail.com>
2023-12-12 10:53:23 +0200

    Th/e2e/wdio (#81)
    * wdio env added

---
+++ test/utils/helpers.ts
@@ -0,0 +1,45 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import path from "path";
+import { GlobalDataEnum } from "./globalDataEnum";
+import { GlobalSelectorEnum } from "./globalSelectorEnum";
+
+export const getSelector = async (selector: string) => {
+  const element = await $(`[data-testid="${selector}"]`);
+  if (!element.isExisting()) {
+    throw new Error(`Element with data-testid "${selector}" not found.`);
+  }
+  return element;
+};
+
+export const uploadFile = async (fileName: string) => {
+  const file = await $('input[type="file"]');
+  const filePath = path.join(__dirname, `/${fileName}`);
+
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  await browser.execute((el) => ((el as any).style.display = "block"), file);
+  file.waitForDisplayed();
+  await file.setValue(filePath);
+};
+
+export const login = async () => {
+  const wallet = await $("button*=MultiversX Web Wallet");
+  const pemBtn = await getSelector(GlobalSelectorEnum.pemBtn);
+  const accesWallet = await getSelector(GlobalSelectorEnum.submitButton);
+
+  await wallet.click();
+  await pemBtn.click();
+  await uploadFile(GlobalDataEnum.pemFile);
+  await accesWallet.click();
+};
+
+export const checkValidity = async (payload: any) => {
+  await expect(payload.selector).toContain(payload.validityMsg);
+};
+
+export const changeNetwork = async () => {
+  const svgElement = await $('svg[data-icon="arrow-down-long"]');
+  const dropdownElement = await $(`div*=${GlobalDataEnum.devnetNetwork}`);
+
+  await svgElement.click();
+  await dropdownElement.click();
+};
