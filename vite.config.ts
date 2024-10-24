08e5c574999cbfaea0b5399ab5d9583d45e83a17
Radu Mojic <radu.mojic@multiversx.com>
2024-10-24 10:16:26 +0300

    - updated vite config - updated @multiversx packages

--- vite.config.ts
+++ vite.config.ts
@@ -1,57 +1,49 @@
 import dns from 'dns';
-import { defineConfig, loadEnv } from 'vite';
-import react from '@vitejs/plugin-react-swc'
-import basicSsl from '@vitejs/plugin-basic-ssl';
-import { nodePolyfills } from 'vite-plugin-node-polyfills';
-// import svgr from 'vite-plugin-svgr';
 import svgr from '@svgr/rollup';
+import basicSsl from '@vitejs/plugin-basic-ssl';
+import react from '@vitejs/plugin-react-swc';
+import { defineConfig } from 'vite';
+import { nodePolyfills } from 'vite-plugin-node-polyfills';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
 // https://vitejs.dev/config/
 dns.setDefaultResultOrder('verbatim');
 
-export default ({ mode }) => {
-  process.env = { ...process.env, ...loadEnv(mode, process.cwd()) };
-
-  return defineConfig({
-    plugins: [
-      svgr(),
-      react(),
-      basicSsl(),
-      tsconfigPaths(),
-      nodePolyfills({
-        globals: { Buffer: true, global: true, process: true },
-      }),
-    ],
-    test: {
-      globals: true,
-      environment: 'jsdom',
-      setupFiles: ['/src/tests/setup.ts'],
+export default defineConfig({
+  plugins: [
+    svgr(),
+    react(),
+    basicSsl(),
+    tsconfigPaths(),
+    nodePolyfills({
+      globals: { Buffer: true, global: true, process: true },
+    }),
+  ],
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: ['/src/tests/setup.ts'],
+  },
+  build: {
+    outDir: 'build',
+  },
+  server: {
+    port: 3001,
+    strictPort: true,
+    https: true,
+    host: 'localhost',
+    hmr: {
+      overlay: false,
     },
-    define: {
-      'process.env': process.env,
+    watch: {
+      usePolling: false,
+      useFsEvents: false,
     },
-    build: {
-      outDir: 'build',
-    },
-    server: {
-      port: 3001,
-      strictPort: true,
-      https: true,
-      host: 'localhost',
-      hmr: {
-        overlay: false,
-      },
-      watch: {
-        usePolling: false,
-        useFsEvents: false,
-      },
-    },
-    preview: {
-      port: 3001,
-      strictPort: true,
-      https: true,
-      host: 'localhost',
-    },
-  });
-};
+  },
+  preview: {
+    port: 3001,
+    strictPort: true,
+    https: true,
+    host: 'localhost',
+  },
+});
